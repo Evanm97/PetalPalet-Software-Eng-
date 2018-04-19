ae6461fabe78c9ca5c8f94aff3c1e9c48b06cc0a
Daniel <daniel.097@hotmail.com>
2018-04-20 00:19:31 +0100

    Update auth.js

--- Public/Javascripts/auth.js
+++ Public/Javascripts/auth.js
@@ -14,8 +14,7 @@ $(document).ready(
                     'password': event.target.inputPassword.value
                 },
                 success: function(token){
-		 swal("Good job!", "You created an account!", "success")
-		 setTimeout(function() {$(location).attr('href', '/users/login' )},3000)	// Redirect to a login page
+		 $(location).attr('href', '/users/login' );	// Redirect to a login page
                 },
                 error: function(errMsg) {
                     swal(
@@ -38,8 +37,7 @@ $(document).ready(
                             'password': event.target.inputPassword.value
                         },
                         success: function(token){
-			     swal("Welcome!", "You're logged in!", "success")
-			     setTimeout(function() {$(location).attr('href', '/' )},3000) // Redirect to logged in page
+                             $(location).attr('href', '/' ); // Redirect to logged in page
                         },
                         error: function(errMsg) {
                             swal(
@@ -51,40 +49,11 @@ $(document).ready(
                     });
                 });
 
-       $("#logout").click(function (event) {
+        $("#logout").click(function (event) {
 
-	Cookies.remove('Authorization');            
-	$(location).attr('href', '/');
-});
-  
-function onSuccess(googleUser) {
-      console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
-    }
-    
-function onFailure(error) {
-      console.log(error);
-    }
-    
-function onSignIn(googleUser) {
-     // Useful data for your client-side scripts:
-     var profile = googleUser.getBasicProfile();
-        console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-        console.log('Full Name: ' + profile.getName());
-        console.log('Given Name: ' + profile.getGivenName());
-        console.log('Family Name: ' + profile.getFamilyName());
-        console.log("Image URL: " + profile.getImageUrl());
-        console.log("Email: " + profile.getEmail());
+          Cookies.remove('Authorization');
+          $(location).attr('href', '/users/login');
+        });
 
-        // The ID token you need to pass to your backend:
-        var id_token = googleUser.getAuthResponse().id_token;
-        console.log("ID Token: " + id_token);
-      };
-   
-function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      console.log('User signed out.');
-    });
-  }
 
 
