e36e36ad44a42c67d569b019fff4bfee9f73a57b
cafferkey <seanog.cafferkey@gmail.com>
2018-04-19 15:07:46 +0100

    Create likes.js

---
+++ Public/Javascripts/likes.js
@@ -0,0 +1,478 @@
+  
+
+var inc1 = (function() {
+
+        var first = true;
+        return function() {
+            first ? liked() : unliked();
+            first = !first;
+        }
+                })();
+
+    function liked() {
+            var thisrating = document.getElementById('rating1');
+            var number = thisrating.innerHTML;
+            number++;
+            thisrating.innerHTML = number;
+            document.getElementById("rating1").style.color = "#e95787";
+            document.getElementById("heart1").style.color = "#e95787";
+    };
+
+    function unliked(){
+
+        var thisrating = document.getElementById('rating1');
+            var number = thisrating.innerHTML;
+            number--;
+            thisrating.innerHTML = number;
+            document.getElementById("rating1").style.color = "#abafc4";
+            document.getElementById("heart1").style.color = "#abafc4";
+
+    };
+
+
+var inc2 = (function() {
+
+      var first = true;
+      return function() {
+        first ? liked2() : unliked2();
+        first = !first;
+      }
+            })();
+
+    function liked2() {
+        var thisrating = document.getElementById('rating2');
+        var number = thisrating.innerHTML;
+        number++;
+        thisrating.innerHTML = number;
+        document.getElementById("rating2").style.color = "#e95787";
+        document.getElementById("heart2").style.color = "#e95787";
+    };
+
+    function unliked2(){
+
+      var thisrating = document.getElementById('rating2');
+        var number = thisrating.innerHTML;
+        number--;
+        thisrating.innerHTML = number;
+        document.getElementById("rating2").style.color = "#abafc4";
+        document.getElementById("heart2").style.color = "#abafc4";
+
+    };
+
+
+
+    var inc3 = (function() {
+
+      var first = true;
+      return function() {
+        first ? liked3() : unliked3();
+        first = !first;
+      }
+            })();
+
+    function liked3() {
+        var thisrating = document.getElementById('rating3');
+        var number = thisrating.innerHTML;
+        number++;
+        thisrating.innerHTML = number;
+        document.getElementById("rating3").style.color = "#e95787";
+        document.getElementById("heart3").style.color = "#e95787";
+    };
+
+    function unliked3(){
+
+      var thisrating = document.getElementById('rating3');
+        var number = thisrating.innerHTML;
+        number--;
+        thisrating.innerHTML = number;
+        document.getElementById("rating3").style.color = "#abafc4";
+        document.getElementById("heart3").style.color = "#abafc4";
+
+    };
+
+
+var inc4 = (function() {
+
+      var first = true;
+      return function() {
+        first ? liked4() : unliked4();
+        first = !first;
+      }
+            })();
+
+    function liked4() {
+        var thisrating = document.getElementById('rating4');
+        var number = thisrating.innerHTML;
+        number++;
+        thisrating.innerHTML = number;
+        document.getElementById("rating4").style.color = "#e95787";
+        document.getElementById("heart4").style.color = "#e95787";
+    };
+
+    function unliked4(){
+
+      var thisrating = document.getElementById('rating4');
+        var number = thisrating.innerHTML;
+        number--;
+        thisrating.innerHTML = number;
+        document.getElementById("rating4").style.color = "#abafc4";
+        document.getElementById("heart4").style.color = "#abafc4";
+
+    };
+
+
+        var inc5 = (function() {
+
+      var first = true;
+      return function() {
+        first ? liked5() : unliked5();
+        first = !first;
+      }
+            })();
+
+    function liked5() {
+        var thisrating = document.getElementById('rating5');
+        var number = thisrating.innerHTML;
+        number++;
+        thisrating.innerHTML = number;
+        document.getElementById("rating5").style.color = "#e95787";
+        document.getElementById("heart5").style.color = "#e95787";
+    };
+
+    function unliked5(){
+
+      var thisrating = document.getElementById('rating5');
+        var number = thisrating.innerHTML;
+        number--;
+        thisrating.innerHTML = number;
+        document.getElementById("rating5").style.color = "#abafc4";
+        document.getElementById("heart5").style.color = "#abafc4";
+
+    };
+
+
+    var inc6 = (function() {
+
+      var first = true;
+      return function() {
+        first ? liked6() : unliked6();
+        first = !first;
+      }
+            })();
+
+    function liked6() {
+        var thisrating = document.getElementById('rating6');
+        var number = thisrating.innerHTML;
+        number++;
+        thisrating.innerHTML = number;
+        document.getElementById("rating6").style.color = "#e95787";
+        document.getElementById("heart6").style.color = "#e95787";
+    };
+
+    function unliked6(){
+
+      var thisrating = document.getElementById('rating6');
+        var number = thisrating.innerHTML;
+        number--;
+        thisrating.innerHTML = number;
+        document.getElementById("rating6").style.color = "#abafc4";
+        document.getElementById("heart6").style.color = "#abafc4";
+
+    };
+
+
+ var inc7 = (function() {
+
+      var first = true;
+      return function() {
+        first ? liked7() : unliked7();
+        first = !first;
+      }
+            })();
+
+    function liked7() {
+        var thisrating = document.getElementById('rating7');
+        var number = thisrating.innerHTML;
+        number++;
+        thisrating.innerHTML = number;
+        document.getElementById("rating7").style.color = "#e95787";
+        document.getElementById("heart7").style.color = "#e95787";
+    };
+
+    function unliked7(){
+
+      var thisrating = document.getElementById('rating7');
+        var number = thisrating.innerHTML;
+        number--;
+        thisrating.innerHTML = number;
+        document.getElementById("rating7").style.color = "#abafc4";
+        document.getElementById("heart7").style.color = "#abafc4";
+
+    };
+
+
+    var inc8 = (function() {
+
+      var first = true;
+      return function() {
+        first ? liked8() : unliked8();
+        first = !first;
+      }
+            })();
+
+    function liked8() {
+        var thisrating = document.getElementById('rating8');
+        var number = thisrating.innerHTML;
+        number++;
+        thisrating.innerHTML = number;
+        document.getElementById("rating8").style.color = "#e95787";
+        document.getElementById("heart8").style.color = "#e95787";
+    };
+
+    function unliked8(){
+
+      var thisrating = document.getElementById('rating8');
+        var number = thisrating.innerHTML;
+        number--;
+        thisrating.innerHTML = number;
+        document.getElementById("rating8").style.color = "#abafc4";
+        document.getElementById("heart8").style.color = "#abafc4";
+
+    };
+
+var inc9 = (function() {
+
+      var first = true;
+      return function() {
+        first ? liked9() : unliked9();
+        first = !first;
+      }
+            })();
+
+    function liked9() {
+        var thisrating = document.getElementById('rating9');
+        var number = thisrating.innerHTML;
+        number++;
+        thisrating.innerHTML = number;
+        document.getElementById("rating9").style.color = "#e95787";
+        document.getElementById("heart9").style.color = "#e95787";
+    };
+
+    function unliked9(){
+
+      var thisrating = document.getElementById('rating9');
+        var number = thisrating.innerHTML;
+        number--;
+        thisrating.innerHTML = number;
+        document.getElementById("rating9").style.color = "#abafc4";
+        document.getElementById("heart9").style.color = "#abafc4";
+
+    };
+
+
+    var inc10 = (function() {
+
+      var first = true;
+      return function() {
+        first ? liked10() : unliked10();
+        first = !first;
+      }
+            })();
+
+    function liked10() {
+        var thisrating = document.getElementById('rating10');
+        var number = thisrating.innerHTML;
+        number++;
+        thisrating.innerHTML = number;
+        document.getElementById("rating10").style.color = "#e95787";
+        document.getElementById("heart10").style.color = "#e95787";
+    };
+
+    function unliked10(){
+
+      var thisrating = document.getElementById('rating10');
+        var number = thisrating.innerHTML;
+        number--;
+        thisrating.innerHTML = number;
+        document.getElementById("rating10").style.color = "#abafc4";
+        document.getElementById("heart10").style.color = "#abafc4";
+
+    };
+
+    var inc11 = (function() {
+
+      var first = true;
+      return function() {
+        first ? liked11() : unliked11();
+        first = !first;
+      }
+            })();
+
+    function liked11() {
+        var thisrating = document.getElementById('rating11');
+        var number = thisrating.innerHTML;
+        number++;
+        thisrating.innerHTML = number;
+        document.getElementById("rating11").style.color = "#e95787";
+        document.getElementById("heart11").style.color = "#e95787";
+    };
+
+    function unliked11(){
+
+      var thisrating = document.getElementById('rating11');
+        var number = thisrating.innerHTML;
+        number--;
+        thisrating.innerHTML = number;
+        document.getElementById("rating11").style.color = "#abafc4";
+        document.getElementById("heart11").style.color = "#abafc4";
+
+    };
+
+
+    var inc12 = (function() {
+
+      var first = true;
+      return function() {
+        first ? liked12() : unliked12();
+        first = !first;
+      }
+            })();
+
+    function liked12() {
+        var thisrating = document.getElementById('rating12');
+        var number = thisrating.innerHTML;
+        number++;
+        thisrating.innerHTML = number;
+        document.getElementById("rating12").style.color = "#e95787";
+        document.getElementById("heart12").style.color = "#e95787";
+    };
+
+    function unliked12(){
+
+      var thisrating = document.getElementById('rating12');
+        var number = thisrating.innerHTML;
+        number--;
+        thisrating.innerHTML = number;
+        document.getElementById("rating12").style.color = "#abafc4";
+        document.getElementById("heart12").style.color = "#abafc4";
+
+    };
+
+var inc13 = (function() {
+
+      var first = true;
+      return function() {
+        first ? liked13() : unliked13();
+        first = !first;
+      }
+            })();
+
+    function liked13() {
+        var thisrating = document.getElementById('rating13');
+        var number = thisrating.innerHTML;
+        number++;
+        thisrating.innerHTML = number;
+        document.getElementById("rating13").style.color = "#e95787";
+        document.getElementById("heart13").style.color = "#e95787";
+    };
+
+    function unliked13(){
+
+      var thisrating = document.getElementById('rating13');
+        var number = thisrating.innerHTML;
+        number--;
+        thisrating.innerHTML = number;
+        document.getElementById("rating13").style.color = "#abafc4";
+        document.getElementById("heart13").style.color = "#abafc4";
+
+    };
+
+
+    var inc14 = (function() {
+
+      var first = true;
+      return function() {
+        first ? liked14() : unliked14();
+        first = !first;
+      }
+            })();
+
+    function liked14() {
+        var thisrating = document.getElementById('rating14');
+        var number = thisrating.innerHTML;
+        number++;
+        thisrating.innerHTML = number;
+        document.getElementById("rating14").style.color = "#e95787";
+        document.getElementById("heart14").style.color = "#e95787";
+    };
+
+    function unliked14(){
+
+      var thisrating = document.getElementById('rating14');
+        var number = thisrating.innerHTML;
+        number--;
+        thisrating.innerHTML = number;
+        document.getElementById("rating14").style.color = "#abafc4";
+        document.getElementById("heart14").style.color = "#abafc4";
+
+    };
+
+        var inc15 = (function() {
+
+      var first = true;
+      return function() {
+        first ? liked15() : unliked15();
+        first = !first;
+      }
+            })();
+
+    function liked15() {
+        var thisrating = document.getElementById('rating15');
+        var number = thisrating.innerHTML;
+        number++;
+        thisrating.innerHTML = number;
+        document.getElementById("rating15").style.color = "#e95787";
+        document.getElementById("heart15").style.color = "#e95787";
+    };
+
+    function unliked15(){
+
+      var thisrating = document.getElementById('rating15');
+        var number = thisrating.innerHTML;
+        number--;
+        thisrating.innerHTML = number;
+        document.getElementById("rating15").style.color = "#abafc4";
+        document.getElementById("heart15").style.color = "#abafc4";
+
+    };
+
+
+        var inc16 = (function() {
+
+      var first = true;
+      return function() {
+        first ? liked16() : unliked16();
+        first = !first;
+      }
+            })();
+
+    function liked16() {
+        var thisrating = document.getElementById('rating16');
+        var number = thisrating.innerHTML;
+        number++;
+        thisrating.innerHTML = number;
+        document.getElementById("rating16").style.color = "#e95787";
+        document.getElementById("heart16").style.color = "#e95787";
+    };
+
+    function unliked16(){
+
+      var thisrating = document.getElementById('rating16');
+        var number = thisrating.innerHTML;
+        number--;
+        thisrating.innerHTML = number;
+        document.getElementById("rating16").style.color = "#abafc4";
+        document.getElementById("heart16").style.color = "#abafc4";
+
+    };
+
